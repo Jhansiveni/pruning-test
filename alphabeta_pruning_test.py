da813e00aeeab59c84557dc14ceff33d7377f4cf
Jhansiveni <jhansivenidesu>
2026-06-28 19:26:20 +0530

    code completed till min player

--- alphabeta_pruning_test.py
+++ alphabeta_pruning_test.py
@@ -26,4 +26,21 @@ def alphabeta(depth, node, isMax, values, alpha, beta):
 
         return best
 
+    # MIN player
+    else:
+        best = 1000
+
+        for i in range(2):
+            val = alphabeta(depth + 1, node * 2 + i,
+                            True, values, alpha, beta)
+
+            best = min(best, val)
+            beta = min(beta, best)
+
+            # Pruning
+            if beta <= alpha:
+                break
+
+        return best
+
 
