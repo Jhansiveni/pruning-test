e47fb41d6ab72adf8cfe091d05f982c809033518
Jhansiveni <jhansivenidesu>
2026-06-28 19:25:03 +0530

    code completed till max player

--- alphabeta_pruning_test.py
+++ alphabeta_pruning_test.py
@@ -1,2 +1,29 @@
 # code for alpha beta pruning test
 
+
+# Simple Alpha-Beta Pruning Code
+
+def alphabeta(depth, node, isMax, values, alpha, beta):
+
+    # Leaf node
+    if depth == 3:
+        return values[node]
+
+    # MAX player
+    if isMax:
+        best = -1000
+
+        for i in range(2):
+            val = alphabeta(depth + 1, node * 2 + i,
+                            False, values, alpha, beta)
+
+            best = max(best, val)
+            alpha = max(alpha, best)
+
+            # Pruning
+            if beta <= alpha:
+                break
+
+        return best
+
+
